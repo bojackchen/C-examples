314d6f500483f2ccab0d0226cc6aa56db2bf1cb0
Bojack F. CHEN <bojackchen@foxmail.com>
2019-09-06 23:12:48 +0800

    problem 3 solved and start problem 4

--- LongestSubstringWithoutRepeatingCharacters/lengthOfLongestSubstring.c
+++ LongestSubstringWithoutRepeatingCharacters/lengthOfLongestSubstring.c
@@ -3,16 +3,23 @@ int lengthOfLongestSubstring(char* s) {
   char* ptr1 = s;
   char* ptr2 = s;
   int length = 1;
+    
+  if (s[0] == 0)
+    return 0;
 
-  for (int i = 1; i < strlen(s); i++) {
-    for (int j = 0; j < i; j++) {
-      if (s[i] == s[j]) {
-        length = (i - j > length) ? i - j : length;
-        ptr1 = s + j + 1;
-        ptr2 = s + i;
+  while (ptr2 != s + strlen(s)) {
+    for (int i = 0; i < ptr2 - ptr1; i++) {
+      if (*ptr2 == ptr1[i]) {
+        length = (ptr2 - ptr1 > length) ? (ptr2 - ptr1) : length;
+        // shrink the sliding window by 1 by moving ptr1 forward by 1
+        ptr1 = ptr1 + 1;
+        // this is to cancel the next statement so that ptr2 does not move
+        ptr2 = ptr2 - 1;
       }
     }
+    // move ptr2 forward by 1
     ptr2 = ptr2 + 1;
   }
+  length = (ptr2 - ptr1 > length) ? (ptr2 - ptr1) : length;
   return length;
 }
---
+++ MedianofTwoSortedArrays/findMedianSortedArrays.c
@@ -0,0 +1,3 @@
+double findMedianSortedArrays(int* num1, int num1Size, int* num2, int num2Size) {
+  double result;
+}
