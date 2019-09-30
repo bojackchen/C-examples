ceea7181dfa7ded442da4ba115c78b16aedc690a
Bojack F. CHEN <bojackchen@foxmail.com>
2019-09-30 23:07:57 +0800

    problem 4 official solution

--- LongestPalindromicSubstring/longestPalindrome.c
+++ LongestPalindromicSubstring/longestPalindrome.c
@@ -1,15 +1,4 @@
 char* longestPalindrome(char* s) {
   char* palindrome = "";
-  int j = 0;
-
-  for (int i = strlen(s); i > 1; i--) {
-    while (s[j] == s[i - j - 1] && j < i - j -1) {
-      j = j + 1;
-    }
-
-    if (j == i / 2) {
-      palindrome = s;
-      palindrome[i] = '\0';
-    }
-  }
+  int slidingWindow = 2;
 }
--- MedianofTwoSortedArrays/findMedianSortedArrays.c
+++ MedianofTwoSortedArrays/findMedianSortedArrays.c
@@ -1,7 +1,40 @@
 /*
  * algorithm complexity should be O(log(m+n))
+ * copy from https://leetcode.com/problems/median-of-two-sorted-arrays/solution/
  */
-
 double findMedianSortedArrays(int* num1, int num1Size, int* num2, int num2Size) {
-  double result;
+  // ensure length(num1) <= length(num2)
+  if (num1Size > num2Size) {
+    int* temp = num1;
+    num1 = num2;
+    num2 = temp;
+    int numTemp = num1Size;
+    num1Size = num2Size;
+    num2Size = numTemp;
+  }
+
+  int iMin = 0, iMax = num1Size, halfLength = (num1Size + num2Size + 1) / 2;
+  while (iMin <= iMax) {
+    int i = (iMin + iMax) / 2;
+    int j = halfLength - i;
+    if (i < iMax && num2[j - 1] > num1[i]) {
+      iMin = i + 1;
+    } else if (i > iMin && num1[i - 1] > num2[j]) {
+      iMax = i - 1;
+    } else {
+      int leftMax = 0;
+      if (i == 0) leftMax = num2[j - 1];
+      else if (j == 0) leftMax = num1[i - 1];
+      else leftMax = num1[i - 1] > num2[j - 1] ? num1[i - 1] : num2[j - 1];
+      if ((num1Size + num2Size) % 2 == 1) return leftMax;
+
+      int rightMin = 0;
+      if (i == num1Size) rightMin = num2[j];
+      else if (j == num2Size) rightMin = num1[i];
+      else rightMin = num2[j] > num1[i] ? num1[i] : num2[j];
+
+      return (leftMax + rightMin) / 2.0;
+    }
+  }
+  return 0.0;
 }
